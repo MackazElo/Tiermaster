6af6155c5541c0d4b4ec371b597482e940d00890
Your Name Here <mackaz2000@gmail.com>
2024-05-26 17:08:12 +0200

    Adding and loading objects and drag and drop works

--- app.js
+++ app.js
@@ -39,6 +39,11 @@ app.get('/create_tierlist', function(req, res) {
     res.render('create_tierlist');
   });
 
+  app.post('/add_object', function(req, res) {
+    var tid = req.body.tid;
+    res.render('add_object', { tid: tid });
+  });
+
 // Trasa do obsługi formularza
 app.post('/submit', function(req, res) {
   var name = req.body.name;
@@ -55,9 +60,42 @@ app.post('/submit', function(req, res) {
       res.status(500).send('Error inserting data');
       return;
     }
+    
+
+    sql = `CREATE TABLE tierlist${result.insertId} (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, url VARCHAR(255) NULL, coverart VARCHAR(255) NULL, tier VARCHAR(255) NULL, colors VARCHAR(255) NULL)`;
+    db.query(sql, function(err, result) {
+        if (err) {
+          console.error('Error inserting data: ' + err.stack);
+          res.status(500).send('Error inserting data');
+          return;
+        }
+    });
     res.send('Data inserted successfully with ID: ' + result.insertId + '<br><a href="./">Back</a>');
-  });
+    });
 });
+
+
+app.post('/submit_object', function(req, res) {
+    var tid = req.body.tid;
+    var name = req.body.name;
+    var url = req.body.url;
+    var coverart = req.body.coverart;
+    var colors = req.body.colors;
+    var tier = "0"
+    
+    var sql = `INSERT INTO tierlist${tid} (name, url, coverart, tier, colors) VALUES (?, ?, ?, ?, ?)`;
+    db.query(sql, [name, url, coverart, tier, colors], function(err, result) {
+      if (err) {
+        console.error('Error inserting data: ' + err.stack);
+        res.status(500).send('Error inserting data');
+        return;
+      }
+      
+  
+      
+      res.send('Data inserted successfully with ID: ' + result.insertId + '<br><a href="./">Back</a>');
+      });
+  });
 
 // Trasa do wyświetlania rekordu o podanym ID
 app.get('/tierlist/:id', function(req, res) {
@@ -75,8 +113,24 @@ app.get('/tierlist/:id', function(req, res) {
       res.status(404).send('Tierlist not found');
       return;
     }
+                var sql2 = `SELECT * FROM tierlist${tierlist_id}`;
+            db.query(sql2, function(err, result2) {
+                if (err) {
+                console.error('Error fetching data: ' + err.stack);
+                res.status(500).send('Error fetching data');
+                return;
+                }
 
-    res.render('tierlist', { tierlist: result[0] });
+                if (result.length === 0) {
+                res.status(404).send('Objects not found');
+                return;
+                }
+                console.log(result2)
+
+                res.render('tierlist', { tierlist: result[0], objects: result2});
+            });
+
+    
   });
 });
 
